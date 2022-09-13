93ac666c0a48354c41c0cda8fe7ae0537b243972
DGSpitzer3090 <wenguhu@gmail.com>
2022-09-13 05:12:39 -0400

    Init Code Push

---
+++ setup.py
@@ -0,0 +1,38 @@
+#@markdown **Setup Environment**
+
+setup_environment = True #@param {type:"boolean"}
+print_subprocess = False #@param {type:"boolean"}
+
+if setup_environment:
+    import subprocess, time
+    print("Setting up environment...")
+    start_time = time.time()
+    all_process = [
+        ['pip', 'install', 'torch==1.12.1+cu113', 'torchvision==0.13.1+cu113', '--extra-index-url', 'https://download.pytorch.org/whl/cu113'],
+        ['conda', 'install', 'git'],
+        ['conda', 'install', '-c', 'conda-forge', 'opencv', '-y'],
+        ['pip', 'install', 'omegaconf==2.2.3', 'einops==0.4.1', 'pytorch-lightning==1.7.4', 'torchmetrics==0.9.3', 'torchtext==0.13.1', 'transformers==4.21.2', 'kornia==0.6.7'],
+        ['git', 'clone', 'https://github.com/deforum/stable-diffusion'],
+        ['pip', 'install', '-e', 'git+https://github.com/CompVis/taming-transformers.git@master#egg=taming-transformers'],
+        ['pip', 'install', '-e', 'git+https://github.com/openai/CLIP.git@main#egg=clip'],
+        ['pip', 'install', 'accelerate', 'ftfy', 'jsonmerge', 'matplotlib', 'resize-right', 'timm', 'torchdiffeq'],
+        ['git', 'clone', 'https://github.com/shariqfarooq123/AdaBins.git'],
+        ['git', 'clone', 'https://github.com/isl-org/MiDaS.git'],
+        ['git', 'clone', 'https://github.com/MSFTserver/pytorch3d-lite.git'],
+        ['pip', 'install', 'IPython'],
+        ['pip', 'install', 'pandas'],
+        ['pip', 'install', 'scikit-image'],
+        ['pip', 'uninstall', 'numpy', '-y'],
+        ['pip', 'install', '-U', 'numpy']
+    ]
+    for process in all_process:
+        running = subprocess.run(process,stdout=subprocess.PIPE).stdout.decode('utf-8')
+        if print_subprocess:
+            print(running)
+    
+    print(subprocess.run(['git', 'clone', 'https://github.com/deforum/k-diffusion/'], stdout=subprocess.PIPE).stdout.decode('utf-8'))
+    with open('k-diffusion/k_diffusion/__init__.py', 'w') as f:
+        f.write('')
+
+    end_time = time.time()
+    print(f"Environment set up in {end_time-start_time:.0f} seconds")
